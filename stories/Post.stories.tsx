58937067a360ad9b4c70707be97bbe8bb1324f7c
TwitoCode <toheebeji@gmail.com>
2022-01-12 13:22:50 -0500

    fixed storybook posts

--- stories/Post.stories.tsx
+++ stories/Post.stories.tsx
@@ -1,9 +1,10 @@
 import { Meta, Story } from "@storybook/react";
 import { Post, PostProps } from "../components/ui/Post";
 
-const props = {
+const props: PostProps = {
 	post: {
 		comments: [],
+		postID: "",
 		content:
 			"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
 		createdAt: "2021-12-22",
@@ -32,12 +33,12 @@ export const Default: Story<PostProps> = () => (
 
 export const PostList = () => (
 	<div className="flex flex-col space-y-6">
-		<Post post={props.post}/>
-		<Post post={props.post}/>
-		<Post post={props.post}/>
-		<Post post={props.post}/>
+		<Post post={props.post} />
+		<Post post={props.post} />
+		<Post post={props.post} />
+		<Post post={props.post} />
 	</div>
-)
+);
 
 const component = {
 	title: "Post",
